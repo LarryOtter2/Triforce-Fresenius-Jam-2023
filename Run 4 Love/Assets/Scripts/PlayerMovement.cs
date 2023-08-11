fc252226b461ac02b1d229b9e1b0438e68801bc9
Moritz Gillrath <mog@gillrath.net>
2023-08-11 20:54:21 +0200

    Plazer Movement

--- Run 4 Love/Assets/Scripts/PlayerMovement.cs
+++ Run 4 Love/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float horizontal;
+    [SerializeField] private float currentSpeed;
+    [SerializeField] private float jumpingPower = 16f;
+    [SerializeField] private float runSpeed = 8f;
+    [SerializeField] private bool isFacingRight;
+    
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundcheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private void Start()
     {
-        
+        currentSpeed = runSpeed;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        currentSpeed = runSpeed;
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        Flip();
+        if(Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+    }
+
+    private bool  isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundcheck.position, 0.2f, groundLayer);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * currentSpeed, rb.velocity.y);
+
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal >  0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
