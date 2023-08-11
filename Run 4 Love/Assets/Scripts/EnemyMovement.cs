15e0cf6c5c2bb1a5eb2cd7980ebde14bdab9f197
R045T-T045T <120365865+R045T-T045T@users.noreply.github.com>
2023-08-11 22:49:27 +0200

    Enemy
    Obstacles Avoiding

--- Run 4 Love/Assets/Scripts/EnemyMovement.cs
+++ Run 4 Love/Assets/Scripts/EnemyMovement.cs
@@ -4,17 +4,71 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public float moveSpeed = 5;
+    float dirX;
 
-    // Start is called before the first frame update
+    [SerializeField]
+    float moveSpeed = 3f;
+
+    Rigidbody2D rb;
+
+    bool facingRight = false;
+
+    Vector3 localScale;
+
+    // Use this for initialization
     void Start()
     {
-        
+        localScale = transform.localScale;
+        rb = GetComponent<Rigidbody2D>();
+        dirX = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (Vector3.right * moveSpeed) * Time.deltaTime;
+      /*
+         if (transform.position.x < 9f)
+            dirX = -1f;
+        else if (transform.position.x > -9f)
+            dirX = 1f;
+      */
     }
-}
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+    }
+
+    void LateUpdate()
+    {
+        CheckWhereToFace();
+    }
+
+    void CheckWhereToFace()
+    {
+        if (dirX > 0)
+            facingRight = true;
+        else if (dirX < 0)
+            facingRight = false;
+
+        if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+            localScale.x *= 1;
+
+        transform.localScale = localScale;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        switch (col.tag)
+            {
+
+            case "LargeJump":
+                rb.AddForce(Vector2.up * 600f);
+                break;
+
+            case "SmallJump":
+                rb.AddForce(Vector2.up * 450f);
+                break;
+                    }
+        }
+    }
