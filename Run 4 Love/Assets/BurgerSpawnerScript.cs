420f14b7533fd13bcf4acea20221795159e2550c
R045T-T045T <120365865+R045T-T045T@users.noreply.github.com>
2023-08-11 20:53:40 +0200

    Enemy
    Enemy Capsule
    
    BurgerSpawn Prefab

---
+++ Run 4 Love/Assets/BurgerSpawnerScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurgerSpawnerScript : MonoBehaviour
+{
+    public GameObject burger;
+    public float spawneRate = 2;
+    private float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnBurger();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawneRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            spawnBurger();
+            timer = 0;
+        }
+
+        
+    }
+
+    void spawnBurger()
+    {
+        Instantiate(burger, transform.position, transform.rotation);
+    }
+}
