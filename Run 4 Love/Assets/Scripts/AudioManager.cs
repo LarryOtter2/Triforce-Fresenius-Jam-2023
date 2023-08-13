c56e10dcd97dc485a6ecbd6e2a51078bc6ff9bbf
Moritz Gillrath <mog@gillrath.net>
2023-08-13 14:19:15 +0200

    Audio Mangaer added

---
+++ Run 4 Love/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,98 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public Sound[] musicSounds, sfxSounds;
+    public AudioSource musicSource, sfxSource;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic("BGM Desert");
+    }
+
+    #region Play
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound not Found");
+        }
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sfx not Found");
+        }
+        else
+        {
+            sfxSource.PlayOneShot(s.clip);
+        }
+    }
+    
+
+    #endregion
+
+
+    #region Stop
+    public void StopMusic(string name)
+    {
+        Sound s = Array.Find(musicSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound not Found");
+        }
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Stop();
+        }
+    }
+    public void StopSFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sfx not Found");
+        }
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Stop();
+        }
+    }
+
+    #endregion
+
+
+
+
+}
